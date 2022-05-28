8f2f361e549b046b19b2c0b4e69c5ecae00ac152
cqMark <mark.baterna@cloudqwest.net>
2022-05-28 09:55:12 +0800

    final

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -90,7 +90,7 @@ export class AuthController {
 
     async makeid(length) {
         var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var characters       = '0123456789';
         var charactersLength = characters.length;
         for ( var i = 0; i < length; i++ ) {
             result += characters.charAt(Math.floor(Math.random() * 
